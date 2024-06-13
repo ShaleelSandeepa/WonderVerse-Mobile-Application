cd0257e241c81662132fad7f85c526d21ef8209c
ShaleelSandeepa <shaleelsandeepa7@gmail.com>
2024-06-13 13:38:09 +0530

    add color file

---
+++ Colors.js
@@ -0,0 +1,50 @@
+export default {
+
+    // primary: '#0D47A1',
+    // category1: '#1976D2',
+    // category2: '#1976D2',
+    // category3: '#1976D2',
+    // category4: '#1976D2',
+    // category5: '#1976D2',
+    // progressBar: 'dodgerblue',
+    // black: 'black',
+    // white: 'white',
+
+    primary: '#1B2631',
+    secondary: '#34495E',
+
+    header: '#1B2631',
+    body: '#34495E',
+    statusBar: '#1B2631',
+    footer: '#1B2631',
+
+    text1: '#FFFFFF',
+    text2: '#1B2631',
+    topic1: '#FFFFFF',
+    topic2: '#1B2631',
+    border1: '#1B2631',
+    border2: '#1B2631',
+    button1: '#1B2631',
+    button2: '#1B2631',
+
+    item1: '#2ECC71',
+    item2: '#3498DB',
+    item3: '#E74C3C',
+    item4: '#F4D03F',
+
+    // primary: '#0D47A1',
+    // category1: '#0D47A1',
+    // category2: '#1565C0',
+    // category3: '#1976D2',
+    // category4: '#1E88E5',
+    // category5: '#2196F3',
+    // progressBar: 'dodgerblue'
+
+    // primary: '#2E2E2E',
+    // category1: '#505252',
+    // category2: '#505252',
+    // category3: '#505252',
+    // category4: '#505252',
+    // category5: '#505252',
+    // progressBar: 'red'
+}
