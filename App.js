775d33206adcc660ce74e2e2ad250eda54f11d94
ShaleelSandeepa <shaleelsandeepa7@gmail.com>
2024-06-13 13:33:51 +0530

    Initiate WonderVerse Mobile Application

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
+import React from 'react';
+import colors from './Colors';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './pages/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          statusBarHidden: false,
+          headerShadowVisible: false,
+          statusBarColor: colors.statusBar,
+          headerTitleAlign: 'center',
+          headerTintColor: 'white',
+          headerStyle: {
+            backgroundColor: colors.header,
+          }
+        }}>
+        <Stack.Group>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: 'Welcome to',
+              headerShown: true,           
+            }}
+          />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
