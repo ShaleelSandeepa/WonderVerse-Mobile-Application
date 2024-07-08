a370513fd1e267cb89d2eec62b5be3f720c7fdfa
ShaleelSandeepa <shaleelsandeepa7@gmail.com>
2024-07-08 21:54:53 +0530

    app.js changes

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import colors from './Colors';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './pages/Home';
+
+import HomeScreen from './screens/HomeScreen';
+import GenerateScreen from './screens/GenerateScreen';
+import StoryScreen from './screens/StoryScreen';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -26,9 +30,25 @@ export default function App() {
         <Stack.Group>
           <Stack.Screen
             name="Home"
-            component={Home}
+            component={HomeScreen}
             options={{
               title: 'Welcome to',
+              headerShown: true,           
+            }}
+          />
+          <Stack.Screen
+            name="Generate"
+            component={GenerateScreen}
+            options={{
+              title: 'Generate Story',
+              headerShown: true,           
+            }}
+          />
+          <Stack.Screen
+            name="Story"
+            component={StoryScreen}
+            options={{
+              title: 'Story',
               headerShown: true,           
             }}
           />
@@ -38,11 +58,11 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#ffff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
